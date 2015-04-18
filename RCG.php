57b19e0e19dbb2816a82ba8482d7ceeb44455da0
pythondude325 <gischeaffer@gmail.com>
2015-04-18 17:17:00 -0500

    Added the <text> functionality

--- RCG.php
+++ RCG.php
@@ -8,6 +8,7 @@ $adjectives = file("Adjectives.txt");
 $nouns = file("Nouns.txt");
 $tags = file("Tags.txt");
 $text = file("Text.txt");
+$domains = file("Domains.txt");
 
 $firstname = substr($firstnames[array_rand($firstnames)], 0, -1);
 $lastname = substr($lastnames[array_rand($lastnames)], 0, -1);
@@ -15,6 +16,7 @@ $verb = substr($verbs[array_rand($verbs)], 0, -1);
 $adjective = substr($adjectives[array_rand($adjectives)], 0, -1);
 $noun = substr($nouns[array_rand($nouns)], 0, -1);
 $title = $verb . " " . $adjective . " " . $noun; 
+$email = $firstname . "." . $lastname . "@" . substr($domains[array_rand($domains)]);
 
 $numoftags = mt_rand(0, 4); // specifies how many tags
 for ($i = -1; $i <= $numoftags; $i++) { // generate the tags
@@ -23,15 +25,18 @@ for ($i = -1; $i <= $numoftags; $i++) { // generate the tags
 }
 
 $numoftext = mt_rand(3, 10); // specifies how many paragraphs of text
-for ($j = -1; $i <= $numoftext; $j++) {
-	$personaltext[$i] = substr($text[array_rand($text)], 0, -1);
+for ($j = -1; $j <= $numoftext; $j++) { // this loop is used to make the paragraphs of text for the <text> item
+	$personaltextlist[$j] = substr($text[array_rand($text)], 0, -1); // puts a random element of $text into $personaltextlist
+	$personaltext = $personaltext . $personaltextlist[$j] . ($j < $numoftext) ? "\n\n" : "";
+	// that last bit adds two new lines if $j < $numoftext
 }
 
 echo "&lt;data&gt;";
 echo "&lt;firstname&gt;" . $firstname . "&lt;/firstname&gt;";
 echo "&lt;lastname&gt;" . $lastname . "&lt;/lastname&gt;";
 echo "&lt;title&gt;" . $title . "&lt;/title&gt;";
-echo "&lt;tags&gt;" . $personaltags . "&lt;/tags&gt;";
+echo "&lt;text&gt;" . $personaltext . "&lt;/text&gt;";
+echo "&lt;email&gt;" . $email . "&lt;/email&gt;"
 echo "&lt;/data&gt;";
 
 
