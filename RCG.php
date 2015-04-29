1565d1b1a08af3c88d6845911afc18f19a85c34e
Graham Scheaffer <gischeaffer@gmail.com>
2015-04-28 19:05:05 -0500

    Fixed a missing semicolon on line 42

--- RCG.php
+++ RCG.php
@@ -28,17 +28,18 @@ for ($i = -1; $i <= $numoftags; $i++) { // generate the tags
 $numoftext = mt_rand(3, 10); // specifies how many paragraphs of text
 for ($j = -1; $j <= $numoftext; $j++) { // this loop is used to make the paragraphs of text for the <text> item
 	$personaltextlist[$j] = substr($text[array_rand($text)], 0, -1); // puts a random element of $text into $personaltextlist
+	$personaltext = ""; // needed a definition
 	$personaltext = $personaltext . $personaltextlist[$j] . ($j < $numoftext) ? "\n\n" : "";
 	// that last bit adds two new lines if $j < $numoftext
 }
 
 // actualy outputs the data
-echo "&lt;data&gt;";
+echo "&lt;data%gt;";
 echo "&lt;firstname&gt;" . $firstname . "&lt;/firstname&gt;";
 echo "&lt;lastname&gt;" . $lastname . "&lt;/lastname&gt;";
 echo "&lt;title&gt;" . $title . "&lt;/title&gt;";
 echo "&lt;text&gt;" . $personaltext . "&lt;/text&gt;";
-echo "&lt;email&gt;" . $email . "&lt;/email&gt;"
+echo "&lt;email&gt;" . $email . "&lt;/email&gt;";
 echo "&lt;/data&gt;";
 
 
