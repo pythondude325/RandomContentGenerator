dff137e76f88eb75bf301df0992647cb50f25c7a
Graham Scheaffer <gischeaffer@gmail.com>
2015-05-03 15:40:43 -0500

    made tha tags more consistent

--- RCG.php
+++ RCG.php
@@ -1,3 +1,5 @@
+#!/bin/php
+
 <?php
 
 // access the files
@@ -19,13 +21,13 @@ $noun = trim($nouns[array_rand($nouns)]);
 $title = $verb . " " . $adjective . " " . $noun; // this strings together the content for the <title> tag
 $email = $firstname . "." . $lastname . "@" . trim($domains[array_rand($domains)]); // does the same exept it's for the <email> tag
 
-$numoftags = mt_rand(0, 4); // specifies how many tags
-for ($i = -1; $i <= $numoftags; $i++) { // generate the tags
-	$curenttagindex = array_rand($tags); // generate a random index from $tags
-	$curenttag = $tags[$curenttagindex]; // get the current tag from $currenttagindex
-	unset($tags[$curenttagindex]); // unset said tag so that i do not get it again when picking (two lines above)
-	$personaltags[$i] = $curenttag;
-	// TODO: implement alphabetical sorting and distinct tags
+$numoftags = mt_rand(1, 4); // specifies how many tags
+for ($i = 0; $i < $numoftags; $i++) { // generate the tags
+	$currenttagindex = array_rand($tags); // generate a random index from $tags
+	$currenttag = $tags[$currenttagindex]; // get the current tag from $currenttagindex
+	// unset($tags[$currenttagindex]); // unset said tag so that i do not get it again when picking (two lines above)
+	$personaltags[$i] = $currenttag;
+	// TODO: string together the tags
 }
 print_r($personaltags); // for debuging
 
@@ -34,7 +36,7 @@ $numoftext = mt_rand(3, 10); // specifies how many paragraphs of text
 for ($j = -1; $j >= $numoftext; $j++) { // this loop is used to make the paragraphs of text for the <text> item
 	$personaltextlist[$j + 1] = trim($text[array_rand($text)]); // puts a random element of $text into $personaltextlist
 	$personaltext = $personaltext . $personaltextlist[$j + 1] . "\n\n";
-	
+	// TODO: finish this
 }
 
 // actualy outputs the data
